d4181b6f7b2c57869cf4f02cb32bd2b80630b513
Keks <keks@htmlacademy.ru>
2023-02-27 15:55:44 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/main.js
@@ -0,0 +1,23 @@
+// Функция, возвращающая случайное целое число из переданного диапазона включительно.
+// Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5_%D1%81%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%BE%D0%B3%D0%BE_%D1%86%D0%B5%D0%BB%D0%BE%D0%B3%D0%BE_%D1%87%D0%B8%D1%81%D0%BB%D0%B0_%D0%B2_%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%D0%BD%D0%BE%D0%BC_%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B2%D0%B0%D0%BB%D0%B5_%D0%B2%D0%BA%D0%BB%D1%8E%D1%87%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE
+
+function getRandomNumber(start, end) {
+  if (start >= 0 && end >= 0) {
+    return Math.floor((Math.random() * (end - start + 1)) + start);
+  }
+}
+
+
+// Функция для проверки максимальной длины строки.
+
+const maxStringLength = function (checkString, maxLength) {
+  if (checkString.length <= maxLength) {
+    return true;
+  }
+
+  return false;
+};
+
+getRandomNumber(0, 5);
+maxStringLength('academy', 140);
+
