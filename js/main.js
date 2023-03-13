932067ea73ed1e9e0882b41b63361920e93f5563
vachagunn <vachikpuper@gmail.com>
2023-03-14 00:03:56 +0500

    added: photo description generation

--- js/main.js
+++ js/main.js
@@ -1,23 +1,76 @@
-// Функция, возвращающая случайное целое число из переданного диапазона включительно.
-// Источник: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5_%D1%81%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%BE%D0%B3%D0%BE_%D1%86%D0%B5%D0%BB%D0%BE%D0%B3%D0%BE_%D1%87%D0%B8%D1%81%D0%BB%D0%B0_%D0%B2_%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%D0%BD%D0%BE%D0%BC_%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B2%D0%B0%D0%BB%D0%B5_%D0%B2%D0%BA%D0%BB%D1%8E%D1%87%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE
+const PHOTO_COUNT = 25;
 
-function getRandomNumber(start, end) {
-  if (start >= 0 && end >= 0) {
-    return Math.floor((Math.random() * (end - start + 1)) + start);
-  }
-}
+const NAMES = [
+  'Вачик',
+  'Яна',
+  'Вика',
+  'Вова',
+  'Саша'
+];
 
+const COMMENTS_ID = [];
 
-// Функция для проверки максимальной длины строки.
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
 
-const maxStringLength = function (checkString, maxLength) {
-  if (checkString.length <= maxLength) {
-    return true;
-  }
+// const checkStringLength = (string, maxLength) => string.length <= maxLength;
 
-  return false;
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 };
 
-getRandomNumber(0, 5);
-maxStringLength('academy', 140);
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 
+const getId = (() => {
+  let id = 1;
+  return () => id++;
+})();
+
+const getCommentId = () => {
+  let id = getRandomPositiveInteger(1, 100);
+  while (COMMENTS_ID.includes(id)) {
+    id = getRandomPositiveInteger(1, 100);
+  }
+  return id;
+};
+
+const generateMessage = () => {
+  const messages = [];
+  for (let i = 0; i < getRandomPositiveInteger(1, 2); i++) {
+    messages.push(getRandomArrayElement(MESSAGES));
+  }
+  return messages;
+};
+
+const createComment = () => {
+  const id = getCommentId();
+  const messages = generateMessage();
+  return {
+    id: id,
+    avatar: `img/avatar-${getRandomPositiveInteger(1, 6)}.svg`,
+    message: messages.join(' '),
+    name: getRandomArrayElement(NAMES),
+  };
+};
+
+const createPhotoDescription = () => {
+  const id = getId();
+  return {
+    id: id,
+    url: `photos/${id}.jpg`,
+    description: `Описание ${id}`,
+    likes: getRandomPositiveInteger(15, 200),
+    comment: Array.from({length: getRandomPositiveInteger(1, 5)}, createComment),
+  };
+};
+
+const photoDescription = Array.from({length: PHOTO_COUNT}, createPhotoDescription);
