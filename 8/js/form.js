9ca0a6ba0a4d7aec41c69267da71c9a69e31143d
Keks <keks@htmlacademy.ru>
2023-04-29 14:36:02 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/form.js
@@ -0,0 +1,51 @@
+import { isEscapeKey } from './util.js';
+
+const form = document.querySelector('#upload-select-image');
+const uploadPhotoInput = document.querySelector('#upload-file');
+const uploadPhotoOverlay = document.querySelector('.img-upload__overlay');
+const uploadCancelBtn = document.querySelector('#upload-cancel');
+const hashtagInput = form.querySelector('.text__hashtags');
+const commentInput = form.querySelector('.text__description');
+
+const onUploadPhotoInputChange = (evt) => {
+  if (evt.target.value) {
+    openUploadOverlay();
+  }
+};
+
+const onUploadPhotoEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeUploadOverlay();
+  }
+};
+
+const onUploadCancelBtnClick = () => {
+  closeUploadOverlay();
+};
+
+const openUploadOverlay = () => {
+  uploadPhotoOverlay.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+
+  document.addEventListener('keydown', onUploadPhotoEscKeydown);
+  uploadCancelBtn.addEventListener('click', onUploadCancelBtnClick);
+  hashtagInput.addEventListener('keydown', (evt) => evt.stopPropagation());
+  commentInput.addEventListener('keydown', (evt) => evt.stopPropagation());
+};
+
+const closeUploadOverlay = () => {
+  uploadPhotoOverlay.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+
+  document.removeEventListener('keydown', onUploadPhotoEscKeydown);
+  uploadCancelBtn.removeEventListener('click', onUploadCancelBtnClick);
+  hashtagInput.removeEventListener('keydown', (evt) => evt.stopPropagation());
+  commentInput.removeEventListener('keydown', (evt) => evt.stopPropagation());
+
+  uploadPhotoInput.value = null;
+};
+
+uploadPhotoInput.addEventListener('change', onUploadPhotoInputChange);
+
+export { openUploadOverlay };
---
+++ 8/js/util.js
@@ -0,0 +1,22 @@
+const getId = () => {
+  let id = 0;
+  return () => {
+    id++;
+    return id;
+  };
+};
+
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+
+const checkStringLenght = (str, maxLenght) => str.length <= maxLenght;
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+export { getId, getRandomPositiveInteger, getRandomArrayElement, checkStringLenght, isEscapeKey };
